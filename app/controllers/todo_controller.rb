ff87a0fdd05f365ef4f74f2773301fa80eb919da
SeirrahBrown <seirrah.brown@gmail.com>
2017-09-30 17:15:52 +0000

    declare instance variable

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,9 @@
 class TodoController < ApplicationController 
     def index 
     end
+    
+    def show 
+        @todo_description="finish homework"
+        @todo_pomodoro_estimate=4
+    end 
 end 
