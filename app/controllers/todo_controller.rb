6f3a1491ff688123cc1df75d20c77f85add90763
SeirrahBrown <seirrah.brown@gmail.com>
2017-10-01 13:36:58 +0000

    add if statements

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,13 +3,28 @@ class TodoController < ApplicationController
     end
     
     def show 
-        todo_id = '2'
+        todo_id = params[:id]
         if todo_id =='1'
-            @todo_description="finish homework"
+            @todo_description="Finish homework"
             @todo_pomodoro_estimate= 4
         elsif todo_id =='2'
-            @todo_description="walk the dog"
+            @todo_description="Walk the dog"
             @todo_pomodoro_estimate=3
+        elsif todo_id =='3'
+            @todo_description= "Clean room"
+            @todo_pomodoro_estimate=2
+        elsif todo_id =='4'
+            @todo_description="Finish essay"
+            @todo_pomodoro_estimate=1
+        elsif todo_id =='5'
+            @todo_description="Call grandma"
+            @todo_pomodoro_estimate=2
+        elsif todo_id =='6'
+            @todo_description="Wash the dishes"
+            @todo_pomodoro_estimate=1
+        elsif todo_id =='7'
+            @todo_description="Go to practice"
+            @todo_pomodoro_estimate=0
         end
     end
     
